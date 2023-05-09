9c045c2ecc0455975ac7fb886f1d33288a86d062
Mattia <mta19@hotmail.it>
2023-05-09 17:52:39 +0200

    day1

--- database.py
+++ database.py
@@ -1,37 +1,74 @@
 import psycopg2
 
+
 def create_room_table():
     query_room = '''
-       CREATE TABLE Room(
+       CREATE TABLE IF NOT EXISTS Room(
            room_id SERIAL PRIMARY KEY NOT NULL,
-           name VARCHAR (100) NOT NULL
+           name_room VARCHAR (100) NOT NULL
        )
     '''
-    
-def create_booking_table():
-    query_booking = '''
-       CREATE TABLE People(
-           people_id SERIAL PRIMARY KEY NOT NULL,
+    cursor = connection.cursor()
+    cursor.execute(query_room)
+    connection.commit()
+
+
+def create_Clients_table():
+    query_Clients = '''
+       CREATE TABLE IF NOT EXISTS Clients(
+           client_id SERIAL PRIMARY KEY NOT NULL,
            first_name VARCHAR (100) NOT NULL,
            second_name VARCHAR (100) NOT NULL,
            dob TIMESTAMP NOT NULL,
            phone_number SMALLINT NOT NULL,
            email VARCHAR (255) NOT NULL
        )
-  '''
+    '''
+    cursor = connection.cursor()
+    cursor.execute(query_Clients)
+    connection.commit()
   
 def create_booking_table():
+    '''PRIMARY KEY (client_id, room_id),
+     FOREIGN KEY (client_id)
+         REFERENCES Clients (client_id),
+     FOREIGN KEY (room_id)
+         REFERENCES Room (room_id)'''
+
     query_booking = '''
-       CREATE TABLE Booking(
+       CREATE TABLE IF NOT EXISTS Booking(
            booking_id SERIAL PRIMARY KEY NOT NULL,
            room_id INTEGER ,
-           people_id INTEGER,
+           client_id INTEGER,
            date_of_booking TIMESTAMP NOT NULL
            CONSTRAINT fk_room
              FOREIGN KEY(room_id) 
              REFERENCES Room(room_id) 
-           CONSTRAINT fk_people
-             FOREIGN KEY(people_id) 
-             REFERENCES People(people_id) 
+           CONSTRAINT fk_clients
+             FOREIGN KEY(client_id) 
+             REFERENCES Client(client_id) 
        )
-  '''
+    '''
+    cursor = connection.cursor()
+    cursor.execute(query_booking)
+    connection.commit()
+
+
+try:
+    connection = psycopg2.connect(user="unicorn_user",
+                                  password="magical_password",
+                                  host="127.0.0.1",
+                                  port="5433",
+                                  database="training")
+    create_booking_table()
+    create_room_table()
+    create_booking_table()
+
+
+except (Exception, Error) as error:
+    print("Error while connecting to PostgreSQL", error)
+finally:
+    if (connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
---
+++ hotel_management.py
@@ -0,0 +1,153 @@
+import psycopg2
+def execute_query (query):
+ try:
+    connection = psycopg2.connect(user="unicorn_user",
+                                   password="magical_password",
+                                   host="127.0.0.1",
+                                   port="5433",
+                                   database="training")
+
+
+    cursor = connection.cursor()
+    cursor.execute(query)
+    record = cursor.fetchall()
+    return record
+
+ except (Exception, Error) as error:
+    print("Error while connecting to PostgreSQL", error)
+ finally:
+      if connection is not None:
+        connection.close()
+
+
+
+
+
+class Clients:
+    def __init__(self,first_name:str,second_name:str,dob,phone_number:int,email:str ):
+        self.first_name=first_name
+        self.second_name=second_name
+        self.dob=dob
+        self.phone_number=phone_number
+        self.email=email
+
+
+class ClientsGateway:
+    def create_client():
+        i = input("inserisci valori first_name,second_name,dob,phone_number,email separati da virgola ")
+        elem=i.split(",")
+        c=Clients(*elem)
+        insert_client(c)
+    def insert_client(self, client:Clients):
+        element = (client.first_name, client.second_name, client.dob,client.phone_number,client.email)
+        insert_query = '''INSERT INTO Clients (first_name,second_name,dob,phone_number,email)  
+                          VALUES(%s,%s,%s,%s,%s)''' % element
+
+        execute_query(insert_query)
+
+#aggiungere controlli input
+    def set_client(self, client_id):
+       while true:
+           insert_query = "SELECT * from Clients WHERE client_id=" + client_id
+           print(execute_query(insert_query))
+           campo_modifica = input(" inserisci nome colonna da modificare")
+           modifica = input(" inserisci nuovo dato ")
+           insert_query = "UPDATE Clients SET " + campo_modifica + " = " + modifica
+           execute_query(insert_query)
+           new_modifica = input("continuare a modificare Y/N?")
+           if (new_modifica == "N"):
+                break
+
+    def delete_client(self, client_id):
+        insert_query = "DELETE FROM Clients WHERE client_id=+" + client_id
+        execute_query(insert_query)
+    def get_client(self, client_id):
+        insert_query = "SELECT * FROM Clients WHERE client_id=+" + client_id
+        print(execute_query(insert_query))
+
+
+class Room:
+    def __init__(self,name_room:str):
+        self.name_room=name_room
+
+class roomGateway:
+    def create_room():
+        i = input("nome Room: ")
+        c=Room(i)
+        insert_room(c)
+    def insert_room(self, client:Clients):
+        element = (Room.name )
+        insert_query = '''INSERT INTO Room (name_room)  
+                          VALUES(%s)''' % element
+
+        execute_query(insert_query)
+
+    def set_room(self, room_id):
+        while true:
+            insert_query = "SELECT * from Room WHERE room_id=" + room_id
+            print(execute_query(insert_query))
+            campo_modifica = input(" inserisci nome colonna da modificare")
+            modifica = input(" inserisci nuovo dato ")
+            insert_query = "UPDATE Room SET " + campo_modifica + " = " + modifica
+            execute_query(insert_query)
+            new_modifica = input("continuare a modificare Y/N?")
+            if (new_modifica == "N"):
+                break
+
+    def delete_room(self, room_id):
+        insert_query = "DELETE FROM Room WHERE room_id=+" + room_id
+        execute_query(insert_query)
+    def get_room(self,room_id):
+        insert_query = "SELECT * FROM Room WHERE room_id=+" + room_id
+        print(execute_query(insert_query))
+class Booking:
+    def __init__(self,room_id,client_id,date_of_booking):
+        self.room_id=room_id
+        self.client_id=client_id
+        self.date_of_booking=date_of_booking
+
+class BookingGateway():
+
+    def create_Booking():
+        i = input("Inserisci dati room_id,client_id,date_of_booking (separati da virgola) ")
+        elem = i.split(",")
+        b = Booking(*elem)
+        insert_Booking(b)
+
+
+    def insert_Booking(self, booking: Booking):
+        element = (booking.room_id,booking.client_id,booking.date_of_booking)
+        insert_query = '''INSERT INTO Booking (name)  
+                          VALUES(%s,%s,%s)''' % element
+
+        execute_query(insert_query)
+
+    def set_Booking(self, booking_id):
+        while true:
+            insert_query = "SELECT * from Booking WHERE booking_id=" + booking_id
+            print(execute_query(insert_query))
+            campo_modifica = input(" inserisci nome colonna da modificare")
+            modifica = input(" inserisci nuovo dato ")
+            insert_query = "UPDATE Booking SET " + campo_modifica + " = " + modifica
+            execute_query(insert_query)
+            new_modifica = input("continuare a modificare Y/N?")
+            if (new_modifica == "N"):
+                break
+
+    def delete_Booking(self, booking_id):
+        insert_query = "DELETE FROM Booking WHERE booking_id=+" + booking_id
+        execute_query(insert_query)
+
+    def get_Booking(self, booking_id):
+        insert_query = '''SELECT first_name.Client,name_room.Room,date_of_booking.Booking
+                           FROM Booking b
+                            INNER JOIN Room r ON b.room_id=r.room_id
+                            INNER JOIN Client c ON c.client_id=c.client_id
+                            WHERE booking_id=''' + booking_id
+        print(execute_query(insert_query))
+
+room_id,client_id,date_of_booking
+class Management():
+   pass
+
+
