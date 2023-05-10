f55b0b0d740c0496ed88a95c6018e5a5b4abd15b
Emanuele213 <vittorius213@gmail.com>
2023-05-10 09:36:08 +0200

    fix bug

--- hotel_management.py
+++ hotel_management.py
@@ -1,4 +1,6 @@
 import psycopg2
+from psycopg2 import Error
+
 def execute_query (query):
  try:
     connection = psycopg2.connect(user="unicorn_user",
@@ -19,10 +21,6 @@ def execute_query (query):
       if connection is not None:
         connection.close()
 
-
-
-
-
 class Clients:
     def __init__(self,first_name:str,second_name:str,dob,phone_number:int,email:str ):
         self.first_name=first_name
@@ -33,21 +31,22 @@ class Clients:
 
 
 class ClientsGateway:
-    def create_client():
-        i = input("inserisci valori first_name,second_name,dob,phone_number,email separati da virgola ")
-        elem=i.split(",")
-        c=Clients(*elem)
-        insert_client(c)
     def insert_client(self, client:Clients):
         element = (client.first_name, client.second_name, client.dob,client.phone_number,client.email)
         insert_query = '''INSERT INTO Clients (first_name,second_name,dob,phone_number,email)  
                           VALUES(%s,%s,%s,%s,%s)''' % element
-
         execute_query(insert_query)
+        
+    def create_client(self):
+        i = input("inserisci valori first_name,second_name,dob,phone_number,email separati da virgola ")
+        elem=i.split(",")
+        c=Clients(*elem)
+        self.insert_client(c)
+    
 
 #aggiungere controlli input
     def set_client(self, client_id):
-       while true:
+       while True:
            insert_query = "SELECT * from Clients WHERE client_id=" + client_id
            print(execute_query(insert_query))
            campo_modifica = input(" inserisci nome colonna da modificare")
@@ -71,19 +70,21 @@ class Room:
         self.name_room=name_room
 
 class roomGateway:
-    def create_room():
-        i = input("nome Room: ")
-        c=Room(i)
-        insert_room(c)
     def insert_room(self, client:Clients):
         element = (Room.name )
         insert_query = '''INSERT INTO Room (name_room)  
                           VALUES(%s)''' % element
 
         execute_query(insert_query)
+        
+    def create_room(self):
+        i = input("nome Room: ")
+        c = Room(i)
+        self.insert_room(c)
+    
 
     def set_room(self, room_id):
-        while true:
+        while True:
             insert_query = "SELECT * from Room WHERE room_id=" + room_id
             print(execute_query(insert_query))
             campo_modifica = input(" inserisci nome colonna da modificare")
@@ -107,14 +108,6 @@ class Booking:
         self.date_of_booking=date_of_booking
 
 class BookingGateway():
-
-    def create_Booking():
-        i = input("Inserisci dati room_id,client_id,date_of_booking (separati da virgola) ")
-        elem = i.split(",")
-        b = Booking(*elem)
-        insert_Booking(b)
-
-
     def insert_Booking(self, booking: Booking):
         element = (booking.room_id,booking.client_id,booking.date_of_booking)
         insert_query = '''INSERT INTO Booking (name)  
@@ -122,8 +115,15 @@ class BookingGateway():
 
         execute_query(insert_query)
 
+    def create_Booking(self):
+        i = input("Inserisci dati room_id,client_id,date_of_booking (separati da virgola) ")
+        elem = i.split(",")
+        b = Booking(*elem)
+        self.insert_Booking(b)
+
+
     def set_Booking(self, booking_id):
-        while true:
+        while True:
             insert_query = "SELECT * from Booking WHERE booking_id=" + booking_id
             print(execute_query(insert_query))
             campo_modifica = input(" inserisci nome colonna da modificare")
@@ -145,9 +145,6 @@ class BookingGateway():
                             INNER JOIN Client c ON c.client_id=c.client_id
                             WHERE booking_id=''' + booking_id
         print(execute_query(insert_query))
-
-room_id,client_id,date_of_booking
+        
 class Management():
    pass
-
-
